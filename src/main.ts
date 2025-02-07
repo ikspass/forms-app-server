a63d2d3757e125fd2d040f4c7892611d0b766f7d
ikspass <moolingtoon@mail.ru>
2025-02-07 21:41:28 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-// import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function start(){
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('api');
-    // const config = new DocumentBuilder()
-    //     .setTitle('Forms App')
-    //     .setDescription('Itransition course project docs')
-    //     .setVersion('1.0.0')
-    //     .build()
-    // const document = SwaggerModule.createDocument(app, config);
-    // SwaggerModule.setup('/api/docs', app, document);
+    // app.setGlobalPrefix('api');
+    const config = new DocumentBuilder()
+        .setTitle('Forms App')
+        .setDescription('Itransition course project docs')
+        .setVersion('1.0.0')
+        .build()
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
 
     await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 }
