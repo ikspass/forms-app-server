ff0043205119967566b830f490ce3ae971876813
ikspass <moolingtoon@mail.ru>
2025-02-10 09:26:15 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function start(){
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    // app.setGlobalPrefix('api');
+    app.setGlobalPrefix('api');
+    const cors = require('cors');
+    app.use(cors({
+        origin: 'http://localhost:3000'
+    }));
     const config = new DocumentBuilder()
         .setTitle('Forms App')
         .setDescription('Itransition course project docs')
